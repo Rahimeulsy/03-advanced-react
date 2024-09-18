6e66a4cbc4f2fc8030f266238fda5fe1724d8155
RahimeUlusoy <rulusoy.2516@gmail.com>
2024-09-18 16:38:59 +0300

    useState Basics-useState with Array-useState with Object

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Starter from './tutorial/01-useState/starter/04-useState-object.jsx';
-import Final from './tutorial/01-useState/final/03-useState-array';
+import Starter from './tutorial/01-useState/starter/04-useState-object';
+import Final from './tutorial/01-useState/final/04-useState-object.jsx';
 
 function App() {
   return (
     <div className='container'>
       <Starter/>
+      {/* <Final/> */}
     </div>
   );
 }
